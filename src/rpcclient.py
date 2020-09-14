12985b166b3d102ab8fcb8d60502364fa081f4ef
Laurin Manke <l.manke@faz.de>
2020-09-14 11:57:36 +0200

    added hashing function to rpcclient

--- src/rpcclient.py
+++ src/rpcclient.py
@@ -1,7 +1,10 @@
 import xmlrpc.client
+import hashlib
+
+hash_object = hashlib.md5(b'Hello World')
 
 def hashFunction():
-    return 1
+    return hash_object.hexdigest()
 
 with xmlrpc.client.ServerProxy('http://localhost:8000') as s:
     print(s.pow(2,3))  # Returns 2**3 = 8
