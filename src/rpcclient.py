26ac8008a39ec00c510812c7873bdf71d71bf4f0
Laurin Manke <l.manke@faz.de>
2020-09-14 12:09:28 +0200

    removed example methods

--- src/rpcclient.py
+++ src/rpcclient.py
@@ -7,9 +7,6 @@ def hashFunction():
     return hash_object.hexdigest()
 
 with xmlrpc.client.ServerProxy('http://localhost:8000') as s:
-    print(s.pow(2,3))  # Returns 2**3 = 8
-    print(s.add(2,3))  # Returns 5
-    print(s.div(5,2)) # Returns 5//2 = 2
     print(hashFunction())
     print(s.hashFunc())
 
--- src/rpcserver.py
+++ src/rpcserver.py
@@ -13,26 +13,10 @@ server = SimpleXMLRPCServer(("localhost", 8000),
                             requestHandler=RequestHandler)
 server.register_introspection_functions()
 
-# Register pow() function; this will use the value of
-# pow.__name__ as the name, which is just 'pow'.
-server.register_function(pow)
-
-# Register a function under a different name
-def adder_function(x,y):
-    return x + y
-server.register_function(adder_function, 'add')
-
 def hashFunction():
     return hash_object_server.hexdigest()
 server.register_function(hashFunction, 'hashFunc')
 
-# Register an instance; all the methods of the instance are
-# published as XML-RPC methods (in this case, just 'div').
-class MyFuncs:
-    def div(self, x, y):
-        return x // y
-
-server.register_instance(MyFuncs())
 print("Server is listening on port 8000...")
 
 # Run the server's main loop
