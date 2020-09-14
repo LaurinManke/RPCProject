c94c4909ce0d840e3aa5245851f6993a77bbd2ad
Laurin Manke <l.manke@faz.de>
2020-09-14 11:54:10 +0200

    initialized hash function on rpcclient

--- src/rpcclient.py
+++ src/rpcclient.py
@@ -1,9 +1,13 @@
 import xmlrpc.client
 
+def hashFunction():
+    return 1
+
 with xmlrpc.client.ServerProxy('http://localhost:8000') as s:
     print(s.pow(2,3))  # Returns 2**3 = 8
     print(s.add(2,3))  # Returns 5
     print(s.div(5,2)) # Returns 5//2 = 2
+    print(hashFunction())
 
 # Print list of available methods
 print(s.system.listMethods())
