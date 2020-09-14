234083641c52ec5e6a642f3ca3994c895d5f8050
Laurin Manke <l.manke@faz.de>
2020-09-14 11:29:51 +0200

    added example code to rpccltient

---
+++ src/rpcclient.py
@@ -0,0 +1,9 @@
+import xmlrpc.client
+
+with xmlrpc.client.ServerProxy('http://localhost:8000') as s:
+    print(s.pow(2,3))  # Returns 2**3 = 8
+    print(s.add(2,3))  # Returns 5
+    print(s.div(5,2)) # Returns 5//2 = 2
+
+# Print list of available methods
+print(s.system.listMethods())
