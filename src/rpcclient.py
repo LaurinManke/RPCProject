4cfc718ae1a905b6e42f7303ac4a6c637b728e19
Laurin Manke <l.manke@faz.de>
2020-09-14 12:24:03 +0200

    added random functionality to rpcserver and rpcclient

--- src/rpcclient.py
+++ src/rpcclient.py
@@ -7,8 +7,11 @@ def hashFunction():
     return hash_object.hexdigest()
 
 with xmlrpc.client.ServerProxy('http://localhost:8000') as s:
+    random_number_client = (s.rnd())
+    print(random_number_client)
     print(hashFunction())
     print(s.hashFunc())
+    print(s.rnd())
 
 # Print list of available methods
 print(s.system.listMethods())
--- src/rpcserver.py
+++ src/rpcserver.py
@@ -1,9 +1,10 @@
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
 import hashlib
+import random
 
 hash_object_server = hashlib.md5(b'Hello World')
-
+random_number_server = float
 # Restrict to a particular path.
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
@@ -12,6 +13,11 @@ class RequestHandler(SimpleXMLRPCRequestHandler):
 server = SimpleXMLRPCServer(("localhost", 8000),
                             requestHandler=RequestHandler)
 server.register_introspection_functions()
+
+def randomFunction():
+    random_number_server = random.random()
+    return random_number_server
+server.register_function(randomFunction, 'rnd')
 
 def hashFunction():
     return hash_object_server.hexdigest()
